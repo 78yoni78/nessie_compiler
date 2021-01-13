f16fe3cc08de005fa0269d5254b0dfcd2f55ce89
Yonatan Reicher <yony252525@gmail.com>
2021-01-13 08:41:06 +0200

    Added Result.fs

---
+++ Nessie/Result.fs
@@ -0,0 +1,8 @@
+﻿module Result
+
+type ResultBuilder() =
+    member t.Return x = Ok x
+    member t.ReturnFrom (x: Result<_, _>) = x
+    member t.Bind(x, f) = Result.bind f x
+
+let result = ResultBuilder()
