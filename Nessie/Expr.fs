3c40ef6a3ae691d71740d4989b83c2c4e8c030c1
Yonatan Reicher <yony252525@gmail.com>
2021-01-13 09:20:19 +0200

    Renamed Function.BodyType to Function.RetType and Added methods for Type

--- Nessie/Expr.fs
+++ Nessie/Expr.fs
@@ -3,13 +3,28 @@
 [<Struct>]
 type Function = 
     { ArgType: Type; Body: Expr; Name: string option }
-    member t.BodyType = t.Body.Type
+    member t.RetType = t.Body.Type
 
 and [<RequireQualifiedAccess>] Type =
     | Specific of Value
     | Type 
     | Int 
     | Func of arg: Type * ret: Type
+    member t.Contains value = 
+        match t, value with
+        | Specific otherValue, value -> otherValue = value
+        | Type, Value.Type _ -> true
+        | Int, Value.Int _ -> true
+        | Func (arg, ret), Value.Func func -> 
+            ret.SuperSet func.RetType && func.ArgType.SuperSet arg
+        | _ -> false
+    member superType.SuperSet subType =
+        match superType, subType with
+        | _ when superType = subType -> true
+        | _, Specific value -> superType.Contains value
+        | Func(arg1, ret1), Func(arg2, ret2) -> 
+            ret1.SuperSet ret2 && arg2.SuperSet arg1
+        | _ -> false
 
 and [<RequireQualifiedAccess>] Value =
     | Type of Type
@@ -21,7 +36,7 @@ and [<RequireQualifiedAccess>] Value =
         match t with
         | Type _ -> Type.Type
         | Int _ -> Type.Int
-        | Func func -> Type.Func (func.ArgType, func.BodyType)
+        | Func func -> Type.Func (func.ArgType, func.RetType)
 
 and [<RequireQualifiedAccess>] Expr =
     | Literal of Value * Token
