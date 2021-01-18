d371c531cb1479b551311736c997804b00922e3c
Yonatan Reicher <yony252525@gmail.com>
2021-01-18 10:08:49 +0200

    new main

--- Nessie/Program.fs
+++ Nessie/Program.fs
@@ -3,25 +3,30 @@
 open System
 
 open Nessie
-open Nessie.TypeCheck
 
 [<EntryPoint>]
 let main argv =
     let text = @"
-let x = 2 in 
-let y = x + 3 in
-    y + x
+let f = x: int -> y: int -> x in
+    f 2 3
 " 
     
     let tokens, errors = Lex.lex text
-
-    let ast = Parse.parse tokens
-
-    let expr = Result.bind TypeCheck.typeCheck (ast |> Result.mapError (fun _ -> Unchecked.defaultof<TypeCheck.TypeError>))
-
     printfn "Tokens: %s" (tokens |> Seq.map string |> String.concat " ")
     printfn "Errors: %A" errors
+
+    let ast = Parse.parse tokens
     printfn "Ast: %A" (ast |> Result.map string |> Result.mapError string)
+
+    let vars = [
+        "int", Value.Type (Type.Int)
+        "+", Value.Func (Function.External (Type.Int, Type.Int, "+"))
+        "-", Value.Func (Function.External (Type.Int, Type.Int, "-"))
+        "*", Value.Func (Function.External (Type.Int, Type.Int, "*"))
+        "/", Value.Func (Function.External (Type.Int, Type.Int, "/"))
+    ]
+    let expr = Result.bind (TypeCheck.typeCheck vars) (ast |> Result.mapError (fun _ -> Unchecked.defaultof<TypeCheck.TypeError>))
     printfn "Expr: %A" (expr |> Result.map string |> Result.mapError string)
     printfn "Type: %A" (expr |> Result.map (fun x -> x.Type |> string) |> Result.mapError string)
+
     0 // return an integer exit code
